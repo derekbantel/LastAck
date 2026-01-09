e7f873e93c71e57bf3f12d60c9f2299f94bc3302
derekbantel <derekbantel@dbantel.com>
2026-01-08 21:52:44 -0600

    started on web server and added daisyui

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./cmd/web/templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./cmd/web/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("/", home)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	fmt.Println("Server started on port 8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
